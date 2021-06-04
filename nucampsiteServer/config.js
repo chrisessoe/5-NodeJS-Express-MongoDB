cf602978f80957842372ef77d0ae46e031b94f52
Chris Essoe <chrisessoe@gmail.com>
2021-06-04 13:58:56 -0700

    Commit

--- nucampsiteServer/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  secretKey: "12345-67890-09876-54321",
-  mongoUrl: "mongodb://localhost:27017/nucampsite",
-  facebook: {
-    clientId: "334969664728392",
-    clientSecret: "4e0189bf1c4775bfb1da3677533c90d5",
-  },
-};
